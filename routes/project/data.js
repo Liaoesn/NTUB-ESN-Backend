21df6c8d01c6d0e8b7726b5035ff1e7474538eea
rinata <rinata.lin011@gmail.com>
2024-09-04 10:55:48 +0800

    修改data.js

--- routes/project/data.js
+++ routes/project/data.js
@@ -3,35 +3,48 @@ const router = express.Router();
 const pool = require('../../lib/db');
 
 router.get('/', async (req, res) => {
-    // 要查專案創建者的編號
     
-    const { year, academic, useno } = req.query;
+    const { year, academic, page = 1, userno } = req.query;
 
     try{
         // 基本查詢語句
-        let query = 'SELECT * FROM `student-project`.`project` WHERE create_id = ?';
-        let params = [userno];
+        let query = `SELECT * FROM \`student-project\`.\`project\` p
+                    JOIN \`student-project\`.\`user\` u
+                    ON p.create_id = u.userno
+                    WHERE 1=1
+                `;
+
+        let params = [];
 
         // 根據是否有 year 來構造查詢條件
         if (year) {
-        query += ' AND LEFT(prono, 3) = ?';
-        params.push(year);
+            query += ' AND LEFT(prono, 3) = ?';
+            params.push(year);
         }
 
         // 根據是否有 academic 來構造查詢條件
         if (academic) {
-        query += ' AND SUBSTRING(prono, 4, 2) = ?';
-        params.push(academic);
+            query += ' AND SUBSTRING(prono, 4, 2) = ?';
+            params.push(academic);
         }
+
+        if (userno) {
+            query += ' AND p.create_id = ?';
+            params.push(userno);
+        }
+
+        // 分頁設定
+        const pageSize = 5;
+        const offset = (page - 1) * pageSize;
+        query += ' LIMIT ? OFFSET ?';
+        params.push(pageSize, offset);
 
         // 執行查詢
         const [results] = await pool.query(query, params);
- 
+
         // 返回結果
         res.json(results);
-
-    }
-    catch (error) {
+    } catch (error) {
         console.error('Error in database query:', error);
         res.status(500).send('Error fetching project data');
     }
