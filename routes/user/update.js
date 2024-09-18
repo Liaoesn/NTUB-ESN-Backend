dd3627750588ec8a3c08cf40f275b802cf5bd212
francisco <francisco940513@gmail.com>
2024-09-18 10:40:51 +0800

    讓資料真的變化時再更新

--- routes/user/update.js
+++ routes/user/update.js
@@ -5,25 +5,40 @@ const pool = require('../../lib/db');
 // 更新使用者資訊（包括停用和改權限）
 router.put('/:userno', async (req, res) => {
     try {
-        console.log('request: ');
-        console.log(req.body);
+        console.log('request: ', req.body);
         const userno = req.params.userno;
         const { permissions, state } = req.body;
 
+        // 查詢當前使用者的資料
+        const [existingRows] = await pool.query('SELECT `permissions`, `state` FROM `student-project`.`user` WHERE `userno` = ?', [userno]);
+        if (existingRows.length === 0) {
+            return res.status(404).json({ error: '未找到使用者' });
+        }
+
+        const existingUser = existingRows[0];
         let query = 'UPDATE `student-project`.`user` SET ';
         let values = [];
+        let fieldsToUpdate = [];
 
-        if (permissions !== undefined) {
-            query += '`permissions` = ?';
+        // 檢查 permissions 是否不同
+        if (permissions !== undefined && permissions !== existingUser.permissions) {
+            fieldsToUpdate.push('`permissions` = ?');
             values.push(permissions);
         }
 
-        if (state !== undefined) {
-            if (values.length > 0) query += ', ';
-            query += '`state` = ?'; 
+        // 檢查 state 是否不同
+        if (state !== undefined && state !== existingUser.state) {
+            fieldsToUpdate.push('`state` = ?');
             values.push(state);
         }
 
+        // 如果沒有需要更新的資料，返回不需要更新的訊息
+        if (fieldsToUpdate.length === 0) {
+            return res.json({ message: '沒有需要更新的資料' });
+        }
+
+        // 組合更新的 SQL 語句
+        query += fieldsToUpdate.join(', ');
         query += ' WHERE `userno` = ?';
         values.push(userno);
 
