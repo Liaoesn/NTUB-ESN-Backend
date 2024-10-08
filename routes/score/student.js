cb5ae710aac13cf92ff23463141412e282654727
francisco <francisco940513@gmail.com>
2024-10-09 00:53:35 +0800

    爆了

--- routes/score/student.js
+++ routes/score/student.js
@@ -14,14 +14,14 @@ router.post('/:prono', async (req, res) => {
             LEFT JOIN collaborator c ON p.prono = c.prono AND c.userno = ?
             LEFT JOIN student s ON p.prono = s.prono
             LEFT JOIN resume r ON s.stuno = r.stuno
-            LEFT JOIN autobiography au ON s.stuno = au.stuno
-            LEFT JOIN studetails d ON s.stuno = d.stuno
-            LEFT JOIN assignment a ON c.colno = a.colno
+			LEFT JOIN autobiography au ON s.stuno = au.stuno
+			LEFT JOIN studetails d ON s.stuno = d.stuno
+            LEFT JOIN assignment a ON c.colno = a.colno AND s.stuno = a.stuno
             LEFT JOIN evaluations e ON a.assno = e.assno
             WHERE p.prono = ?
             ORDER BY e.evano ASC
         `, [userno, prono]);
-
+        
 
         // 返回查詢結果
         res.json({ data: rows });
