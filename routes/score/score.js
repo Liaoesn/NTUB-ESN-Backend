949b924c922aa6fbf8d5719b12c9410889562e41
francisco <francisco940513@gmail.com>
2024-11-17 08:26:53 +0800

    score

--- routes/score/score.js
+++ routes/score/score.js
@@ -3,20 +3,20 @@ const router = express.Router();
 const pool = require('../../lib/db');
 
 // 提交最終排序
-router.post('/submit', async (req, res) => {
-  const { score } = req.body; // score 為前端傳入的陣列
-  if (!Array.isArray(score) || score.length === 0) {
+router.post('/', async (req, res) => {
+  const { data } = req.body; // score 為前端傳入的陣列
+  if (!Array.isArray(data) || score.length === 0) {
     return res.status(400).json({ error: '無效的分數數據' });
   }
 
   try {
     // 用來批量更新每條評分記錄
     const updatePromises = score.map(item => {
-      const { eva_no, memo, score } = item; // 從前端獲取 no、memo 和 score
+      const { id, score, memo } = item; // 從前端獲取 no、memo 和 score
       const evalUpdateQuery = 'UPDATE ESN.evaluations SET score = ?, memo = ? WHERE eva_no = ?';
 
       // 返回 pool.query 的 Promise
-      return pool.query(evalUpdateQuery, [score, memo, eva_no]);
+      return pool.query(evalUpdateQuery, [score, memo, id]);
     });
 
     // 等待所有更新完成
